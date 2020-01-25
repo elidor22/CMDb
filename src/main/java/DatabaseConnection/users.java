e73a1044089192bb5d2cee6df96722b84d34495f
elidor22 <elidorv@outlook.com>
2020-01-25 19:20:36 +0100

    Mapped users class

---
+++ src/main/java/DatabaseConnection/users.java
@@ -0,0 +1,38 @@
+package DatabaseConnection;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class users {
+    private int id;
+    private String username;
+    private String password;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
